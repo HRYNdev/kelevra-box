55719605295d0ddf57452e74135563463007fcb6
HRYNdev <vladimirshev10@gmail.com>
2026-08-03 18:22:08 +0500

    клиент сообщает панели устройство: учёт перестаёт быть слепым к нашему клиенту

--- app/src/main/java/io/nekohasekai/sfa/utils/HTTPClient.kt
+++ app/src/main/java/io/nekohasekai/sfa/utils/HTTPClient.kt
@@ -1,7 +1,9 @@
 package io.nekohasekai.sfa.utils
 
+import android.os.Build
 import io.nekohasekai.libbox.Libbox
 import io.nekohasekai.sfa.BuildConfig
+import io.nekohasekai.sfa.Kelevra
 import io.nekohasekai.sfa.ktx.unwrap
 import java.io.Closeable
 import java.util.Locale
@@ -32,6 +34,14 @@ class HTTPClient : Closeable {
         val request = client.newRequest()
         request.setUserAgent(userAgent)
         request.setURL(url)
+        // Панель ведёт учёт устройств по этим заголовкам. Без них наш клиент для неё
+        // безымянный, и ограничение по числу устройств не работает в принципе.
+        if (url.contains(Kelevra.SUBSCRIPTION_HOST)) {
+            request.setHeader("x-hwid", Kelevra.deviceId)
+            request.setHeader("x-device-os", "Android")
+            request.setHeader("x-ver-os", Build.VERSION.RELEASE ?: "")
+            request.setHeader("x-device-model", Build.MODEL ?: "")
+        }
         val response = request.execute()
         return response.content.unwrap
     }
